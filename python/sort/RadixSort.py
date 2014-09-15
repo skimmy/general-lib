e4cf7dbd10d6b926b1cc25a3f2e2856fc5318014
Michele Schimd <michele.schimd@gmail.com>
2014-09-16 00:49:43 +0200

    minor advance on radix sort (pyton)

--- python/sort/RadixSort.py
+++ python/sort/RadixSort.py
@@ -1,11 +1,20 @@
 #! /usr/bin/python
 
+import math
+
+def insetionSort(toSort):
+    sortedOut = []
+    return sortedOut
+
 def bucketSort(toSort):
     sortedOut = []
     return sortedOut
 
-def radixSort(toSort):
+def radixSort(toSort, nbuckets=16):
     sortedOut = []
+    groupBitsCount = math.log(nbuckets, 2)
+    for i in range(nbuckets):
+        pass
     return sortedOut
 
 if __name__ == "__main__":
