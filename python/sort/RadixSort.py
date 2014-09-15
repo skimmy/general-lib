d8c1c669d1fba7f1f421128523468c8bb42d52b0
Michele Schimd <michele.schimd@gmail.com>
2014-09-15 20:47:19 +0200

    added stub for radix/bucket sort

---
+++ python/sort/RadixSort.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python
+
+def bucketSort(toSort):
+    sortedOut = []
+    return sortedOut
+
+def radixSort(toSort):
+    sortedOut = []
+    return sortedOut
+
+if __name__ == "__main__":
+    print("[DEMO] - RadixSort.py")
